247ffd7ca0a8e5988fa72c34406de1f36acf0589
LiangHaoXiang <1013568397@qq.com>
2021-08-14 11:50:51 +0800

    Initial Commit

---
+++ OpenGL_Test26_DefferedShading/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  OpenGL_Test26_DefferedShading
+//
+//  Created by Haoxiang Liang on 2021/8/14.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
